6be925e6d1e281a163998bab5c7422d03cf4cc6e
Rainer Ramos <rainermr89@hotmail.com>
2021-08-26 15:02:30 -0300

    Resolução exercício  - LinearLayout, CheckBox e ImageView

--- app/src/main/java/com/rainerramos/elementos_2/MainActivity.kt
+++ app/src/main/java/com/rainerramos/elementos_2/MainActivity.kt
@@ -2,10 +2,23 @@ package com.rainerramos.elementos_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkColor.setOnClickListener {
+            if (checkColor.isChecked) {
+                imageColor.setColorFilter(resources.getColor(R.color.colorAccent))
+            } else {
+                imageColor.setColorFilter(resources.getColor(R.color.black))
+            }
+        }
+
     }
+
+
+
 }
